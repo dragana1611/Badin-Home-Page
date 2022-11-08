b111fe99efee3337d971b292f5e3dd68cdfff4c7
Dragana Petrovic <dragana.popov-petrovic@badin.rs>
2022-11-08 17:07:47 +0100

    inprogress

--- script.js
+++ script.js
@@ -8,19 +8,18 @@ const navMenu = document.getElementById("nav-menu");
 const menuItems = document.querySelector("#overlay");
 console.log(menuItems);
 
-const toggleMenu = (e) =>{
+const toggleMenu = (e) => {
   e.preventDefault();
   menuItems.classList.toggle("open");
   menuBtn.classList.toggle("full-menu");
-}
+};
 
 menuBtn.addEventListener("click", toggleMenu);
 document.querySelectorAll(".navigation__link").forEach((lnk) => {
   lnk.addEventListener("click", () => {
-    menuBtn.classList.remove(".open");    
+    menuBtn.classList.remove(".open");
   });
 });
-
 
 //scroll - change font color
 
@@ -31,6 +30,22 @@ document.querySelectorAll(".navigation__link").forEach((lnk) => {
 //     //show button
 //     hamburger.style.backgroundColor = 'white'
 //     hamburger.style.color = 'white'
-//   } 
+//   }
 // }
 // document.addEventListener("scroll", handleScroll);
+
+//gallery scroll-up
+const appear = document.querySelectorAll(".appear");
+console.log(appear);
+const cb = (entries) => {
+  entries.forEach((entry) => {
+    entry.isIntersecting
+      ? entry.target.classList.add("inview")
+      : entry.target.classList.remove("inview");
+  });
+};
+const io = new IntersectionObserver(cb);
+for (const el of appear) {
+  io.observe(el);
+}
+console.log(io);
