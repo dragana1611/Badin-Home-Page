8d3878218472c03a3ff6c07686b278af3a46858b
Dragana Petrovic <dragana.popov-petrovic@badin.rs>
2022-11-16 17:56:12 +0100

    inprogress

--- script.js
+++ script.js
@@ -1,54 +1,92 @@
 "use strict";
 
 //animated hamburger button
-const menuBtn = document.querySelector("#toggle");
-console.log("menuBtn", menuBtn);
+const menuHamburgerBtn = document.querySelector("#toggle");
+//console.log("menuHamburgerBtn", menuHamburgerBtn);
 
 const hamburger = document.getElementById("hamburger");
-console.log(hamburger);
+//console.log(hamburger);
 
 const navMenu = document.getElementById("nav-menu");
 
 const menuItems = document.querySelector("#overlay");
-console.log(menuItems);
+//console.log(menuItems);
 
 const toggleMenu = (e) => {
   e.preventDefault();
   menuItems.classList.toggle("open");
-  menuBtn.classList.toggle("full-menu");
+  menuHamburgerBtn.classList.toggle("full-menu");
 };
 
-menuBtn.addEventListener("click", toggleMenu);
+menuHamburgerBtn.addEventListener("click", toggleMenu);
 document.querySelectorAll(".navigation__link").forEach((lnk) => {
   lnk.addEventListener("click", () => {
-    menuBtn.classList.remove(".open");
+    menuHamburgerBtn.classList.remove(".open");
   });
 });
 
 //scroll - change color of hamburger button
 
 const rootElement = document.documentElement;
-
 let lastKnownScrollPosition = 0;
+const darkBgSections = document.querySelectorAll(".dark-bg");
+const darkBgSectionsArr = [...darkBgSections];
 
 const handleScrollHamburgerButton = () => {
   lastKnownScrollPosition = Math.ceil(window.scrollY);
-  console.log("lastKnownScrollPosition-Y", lastKnownScrollPosition);
+  const menuBtn = document.querySelector(".navigation__button");
+  const menuBtnScrollHeight =
+    Math.ceil(menuBtn.getBoundingClientRect().y) + menuBtn.offsetHeight;
 
-  const sectionHero = document.querySelector(".hero");
+  const foundDarkSection = () => {
+    let foundSection = false;
+    darkBgSectionsArr.forEach((section) => {
+      const sectionTop = Math.abs(section.offsetTop) - menuBtnScrollHeight;
+      const sectionBottom =
+        Math.abs(section.offsetTop) +
+        section.offsetHeight -
+        menuBtnScrollHeight;
 
-  const sectionHeroScrollHeight = sectionHero.scrollHeight;
-  console.log("sectionHeroScrollHeight", sectionHeroScrollHeight);
+      if (
+        lastKnownScrollPosition >= sectionTop &&
+        lastKnownScrollPosition < sectionBottom
+      ) {
+        foundSection = true;
+      }
+    });
+    return foundSection;
+  };
+  //console.log(foundDarkSection());
+  if (foundDarkSection()) {
+    menuBtn.style.color = "red";
+    // console.log("if");
+  } else {
+    menuBtn.style.color = "green";
+    // console.log("else");
+  }
 
-  const sectionHeroClientHeight = sectionHero.clientHeight;
-  console.log("sectionHeroClientHeight", sectionHeroClientHeight);
+  //
+  // if (
+  //   lastKnownScrollPosition >= sectionTop &&
+  //   lastKnownScrollPosition < sectionBottom
+  //   && !foundSection
+  // ) {
+  //   foundSection = true;
+  //   menuBtn.style.color = "red"
+  // } else {
+  //   menuBtn.style.color = "green";
+  //   // console.log("else");
+  // };
 
-  const menuBtnScrollHeight = menuBtn.scrollHeight;
-  console.log("menuBtnScrollHeight", menuBtnScrollHeight);
+  //
 
-  menuBtnScrollHeight > 0 && menuBtnScrollHeight < sectionHeroScrollHeight
-    ? (menuBtn.style.color = "white")
-    : (menuBtn.style.color = "green");
+  // console.log({ menuBtnScrollHeight });
+  // console.log(
+  //   "menuBtn.getBoundingClientRect().y",
+  //   menuBtn.getBoundingClientRect().y
+  // );
+  // console.log("menuBtn.offsetHeight", menuBtn.offsetHeight);
+  // console.log("menuBtn.offsetTop", menuBtn.offsetTop);
 };
 
 window.addEventListener("scroll", (e) => handleScrollHamburgerButton());
@@ -77,10 +115,10 @@ const scrollToTop = () => {
   const progressValue = document.getElementById("progress-value");
 
   let position = rootElement.scrollTop;
-  console.log("position=", position);
+  //console.log("position=", position);
 
   let calcHeight = rootElement.scrollHeight - rootElement.clientHeight;
-  console.log("calcHeight=", calcHeight);
+  // console.log("calcHeight=", calcHeight);
 
   let scrollValue = Math.round((position * 100) / calcHeight);
 
@@ -97,106 +135,93 @@ const scrollToTop = () => {
 window.addEventListener("scroll", (e) => scrollToTop());
 window.addEventListener("load", (e) => scrollToTop());
 
-//gallery lightbox
+//gallery picture preview modal
 const images = document.querySelectorAll(".home-img-dt");
-console.log(images);
+//console.log(images);
 let imgArray = [...images];
-console.log(imgArray);
+// console.log(imgArray);
 
 const overlays = document.querySelectorAll(".gallery__overlay");
+let overlaysArray = [...overlays];
 
-const handleCreateLightbox = (e) => {
-  const linkImage = e.target.getAttribute("src");
+const handleCreatePicturePriview = (e) => {
+  const srcImage = e.target.previousElementSibling.getAttribute("src");
+  const galleryContainer = document.querySelector(".gallery__container");
+  console.log(galleryContainer);
+  console.log(srcImage);
 
-  const lightboxContent = `
-              <div class="lightbox">
-                <div class="lightbox-content">
-                  <button class="lr-button lr-button--left">
-                    <i class="fa fa-angle-left lightbox-prev"></i>
-                  </button>
-                  <img
-                    src="${linkImage}"
-                    alt=""
-                    class="lightbox-image"
-                  />
-                  <button class="lr-button lr-button--right">
-                    <i class="fa fa-angle-right lightbox-next"></i>
-                  </button>
-                </div>
-              </div>
-              `;
-  document.body.insertAdjacentHTML("beforeend", lightboxContent);
+  // console.log(e.target);
+  const picturePriviewContent = `
+    <div class="picturePriview">
+      <button class="arrow-btn arrow-btn--left"">
+        <i class="fa-solid fa-left-long arrow picturePriview-prev"></i>
+      </button>
+      <div class="picturePriview-content">        
+      <img
+        src="${srcImage}"
+        alt=""
+        class="picturePriview-image"
+      />                  
+      </div>
+      <button class="arrow-btn arrow-btn--right">
+          <i class="fa-solid fa-right-long arrow picturePriview-next"></i>
+      </button>
+    </div>
+    `;
+  
+  galleryContainer.insertAdjacentHTML("beforeend", picturePriviewContent);
 };
 
-window.addEventListener("load", (e) => {
-  imgArray.forEach((item) => {
-    console.log(item);
-    // const nextSibling = item.nextElementSibling;
-    // console.log(nextSibling)
-    item.nextElementSibling.addEventListener("click", handleCreateLightbox);
-  });
+overlaysArray.forEach((item) => {
+   console.log(item);
+  item.addEventListener("click", handleCreatePicturePriview);
+});
 
-  let index = 0;
+let index = 0;
 
-  document.addEventListener("click", handleOutLightbox);
-
-  function handleOutLightbox(e) {
-    const lightImage = document.querySelector(".lightbox-image");
-
-    let imageSrc = "";
-    if (!lightImage) return;
-
-    if (e.target.matches(".lightbox")) {
-      const body = e.target.parentNode;
-      body.removeChild(e.target);
-    } else if (e.target.matches(".lightbox-next")) {
-      imageSrc = lightImage.getAttribute("src");
-
-      index = imgArray.findIndex(
-        (item) => item.getAttribute("src") === imageSrc
-      );
-
-      index = index + 1;
-
-      firstImage = 0;
-      if (index > images.length - 1) {
-        index = firstImage;
-      }
-
-      ChangeLinkImage(index, lightImage);
-    } else if (e.target.matches(".lightbox-prev")) {
-      imageSrc = lightImage.getAttribute("src");
-
-      index = imgArray.findIndex(
-        (item) => item.getAttribute("src") === imageSrc
-      );
-
-      index = index - 1;
-
-      lastImage = images.length - 1;
-      if (index < 0) {
-        index = lastImage;
-      }
-
-      ChangeLinkImage(index, lightImage);
-    }
-  }
+const handleOutPicturePriview = (e) => {
+  const lightImage = document.querySelector(".picturePriview-image");
+  // console.log(lightImage);
+  let imageSrc = "";
 
   const ChangeLinkImage = (index, lightImage) => {
     const newLink = imgArray[index].getAttribute("src");
     lightImage.setAttribute("src", newLink);
   };
-});
 
-window.addEventListener("click", (e) => {
-  const root = document.documentElement;
-  root.dataset.lightbox = "off";
-  e.stopPropagation();
-});
+  if (!lightImage) return;
 
-// const lightImage = document.querySelector(".lightbox-image");
-// lightImage.addEventListener("click", (e) => {
-//   const root = document.documentElement;
-//   root.dataset.lightbox = "off";
-//   e.stopPropagation();
-// });
+  if (e.target.matches(".picturePriview")) {
+    const body = e.target.parentNode;
+    body.removeChild(e.target);
+  } else if (e.target.matches(".picturePriview-next")) {
+    imageSrc = lightImage.getAttribute("src");
+
+    index = imgArray.findIndex((item) => item.getAttribute("src") === imageSrc);
+
+    index = index + 1;
+
+    let firstImage = 0;
+    if (index > images.length - 1) {
+      index = firstImage;
+    }
+
+    ChangeLinkImage(index, lightImage);
+  } else if (e.target.matches(".picturePriview-prev")) {
+    imageSrc = lightImage.getAttribute("src");
+
+    index = imgArray.findIndex((item) => item.getAttribute("src") === imageSrc);
+
+    index = index - 1;
+
+    let lastImage = images.length - 1;
+    if (index < 0) {
+      index = lastImage;
+    }
+
+    ChangeLinkImage(index, lightImage);
+  }
+};
+
+window.addEventListener("click", handleOutPicturePriview);
+
